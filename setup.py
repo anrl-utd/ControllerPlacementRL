ebdceb0d02238af466ff9716dd204adf8a6024dd
Usaid M <UsaidPro@gmail.com>
2019-11-01 13:52:46 -0500

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ from setuptools import setup
 
 setup(name='controller_env',
     version='0.0.1',
-    install_requires=['gym']	#And any other dependencies required
+    install_requires=['gym', 'networkx', 'matplotlib']	#And any other dependencies required
 )
