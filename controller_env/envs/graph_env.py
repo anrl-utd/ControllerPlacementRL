7b7dc7cd15c26b123006c9f5bd67ddcd0f1986d3
Usaid M <UsaidPro@gmail.com>
2019-11-01 17:26:04 -0500

    Added distance weights to edges

--- controller_env/envs/graph_env.py
+++ controller_env/envs/graph_env.py
@@ -32,21 +32,23 @@ class ControllerEnv(gym.Env):
 		"""Renders the environment once"""
 		print("Rendered enviroment")
 
-def generateGraph(numClusters, numNodes, prob=0.2, draw=True):
+def generateGraph(num_clusters, num_nodes, prob=0.2, weight_low=0, weight_high=100, draw=True):
 	"""Generates graph given number of clusters and nodes
 	Args:
-		numClusters: Number of clusters
-		numNodes: Number of nodes
+		num_clusters: Number of clusters
+		num_nodes: Number of nodes
 		prob: Parameter for graph (probability of triangle after adding edge)
+		weight_low: Lowest possible weight for edge in graph
+		weight_high: Highest possible weight for edge in graph
 		draw: Whether or not to show graph (True indicates to show)
 	
 	Returns:
 		Graph with nodes in clusters
 	"""
-	graph = nx.powerlaw_cluster_graph(numNodes,3, prob, random.seed(a = None, version = 2))
+	graph = nx.powerlaw_cluster_graph(num_nodes,3, prob, random.seed(a = None, version = 2))
 
 	traversal = list(nx.bfs_tree(graph, source = 0))    # get a bft of the graph in a list
-	array_traversal = np.array_split(traversal, numClusters)      # split the bft list into equal parts
+	array_traversal = np.array_split(traversal, num_clusters)      # split the bft list into equal parts
 
 	pos = nx.spring_layout(graph)   # get the positions of the nodes of the graph
 	for index, the_traversal in enumerate(array_traversal): # for each cluster and index of the graph
@@ -57,10 +59,15 @@ def generateGraph(numClusters, numNodes, prob=0.2, draw=True):
 		randColor = "#" + str(random.randint(0, 999999)).zfill(6)   # get a random hexadecimal colour
 		if draw:
 			nx.draw_networkx_nodes(graph.subgraph(the_traversal), subpos, node_color=randColor, label = "Cluster " + str(index))  # draw an individual cluster
+	
+	for edge in graph.edges:
+		graph.add_edge(edge[0], edge[1], weight=random.randint(weight_low, weight_high))
+	
 	if draw:
 		nx.draw_networkx_edges(graph, pos, graph.edges())        # draw the edges of the graph
-		nx.draw_networkx_labels(graph, pos)                      # draw the labels of the graph
-
+		nx.draw_networkx_labels(graph, pos)                      # draw  the labels of the graph
+		edge_labels = nx.get_edge_attributes(graph,'weight')
+		nx.draw_networkx_edge_labels(graph,pos,edge_labels=edge_labels) # draw the edge weights of the graph
 		plt.savefig("path.png")
 		plt.draw()
 		plt.legend()
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 import controller_env
 from controller_env.envs.graph_env import generateGraph
 
-graph = generateGraph(3, 12, draw=False)
+graph = generateGraph(3, 12, draw=True)
 env = gym.make('Controller-v0', graph=graph)
 env.step(0)
 env.reset()
