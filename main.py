016e2f8cc1a96cfd74a148f3cfc93d3e809cdd2f
Usaid M <UsaidPro@gmail.com>
2020-02-14 12:49:46 -0600

    Graph is now pickled, so KeyboardInterrupts and restarting the training will not change graph

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """
 Main code to create graph and run agent
 """
+import os
+os.environ['FOR_DISABLE_CONSOLE_CTRL_HANDLER'] = '1'
 import gym
 import numpy as np
 import controller_env
@@ -14,64 +16,73 @@ from stable_baselines import PPO1
 import optuna
 import shutil
 import pickle
-import os
+import sys
+import signal
 
-def optimize_algorithm(trial, graph, clusters, env_name='Controller-Direct-v0'):
+def optimize_algorithm(trial, graph, clusters, pos, env_name='Controller-Direct-v0'):
 	"""Optimizes an algorithm using Optuna (tries out different parameters)"""
 	#TODO: Ensure early pruning of trials occurs to speed up optimization (Tensorflow hook?)
+	try:
+		model_params = {
+			'gamma': trial.suggest_loguniform('gamma', 0.9, 0.999),
+			'adam_epsilon': trial.suggest_uniform('adam_epsilon', 1e-8, 1e-4),
+			'lam': trial.suggest_uniform('lam', 0.9, 1),
+			'clip_param': trial.suggest_uniform('clip', 0.1, 0.4)
+		}
 
-	model_params = {
-		'gamma': trial.suggest_loguniform('gamma', 0.9, 0.999),
-		'adam_epsilon': trial.suggest_uniform('adam_epsilon', 1e-8, 1e-4),
-		'lam': trial.suggest_uniform('lam', 0.9, 1),
-		'clip_param': trial.suggest_uniform('clip', 0.1, 0.4)
-	}
+		#Nudging environment
+		env = gym.make('Controller-Direct-v0', graph=graph, clusters=clusters, pos=pos)
+		#Agent
+		model = PPO1('MlpPolicy', env, tensorboard_log='train_log', verbose=0, **model_params)
+		# Train the agent
+		model.learn(total_timesteps=int(1e4))
 
-	#Nudging environment
-	env = gym.make('Controller-Direct-v0', graph=graph, clusters=clusters, pos=pos)
-	#Agent
-	model = PPO1('MlpPolicy', env, tensorboard_log='train_log', verbose=0, **model_params)
-	# Train the agent
-	model.learn(total_timesteps=int(1e4))
+		loops = 0
+		obs = env.reset()
+		reward = 0 #We want the last reward to be minimal (perhaps instead do cumulative?)
+		while loops < 100:
+			action, states = model.predict(obs)
+			(obs, reward, _, _) = env.step(action)
+			loops += 1
+		trial.report(reward)
 
-	loops = 0
-	obs = env.reset()
-	reward = 0 #We want the last reward to be minimal (perhaps instead do cumulative?)
-	while loops < 100:
-		action, states = model.predict(obs)
-		(obs, reward, _, _) = env.step(action)
-		loops += 1
-	trial.report(reward)
-
-	if(reward == 100000):
-		#Since I am unsure if Optuna does multiprocessing, I'm going to search the logging path for the last-logged
-		#and delete it if it isn't relevant
-		path = os.path.abspath(os.getcwd()) + '/train_log'
-		list_subfolders_with_paths = [f.path for f in os.scandir(path) if f.is_dir()]
-		paths = [int(f.split('/')[-1].split('_')[-1]) for f in list_subfolders_with_paths] #Can combine with previous line, but wanted readability
-		try:
-			shutil.rmtree(path + '/PPO1_' + str(sorted(paths)[-1]))
-		except OSError as e:
-			print("Error removing log file")
-	return reward #Optuna by default minimizes, so changing this to positive distance
+		if(reward == 100000):
+			#Since I am unsure if Optuna does multiprocessing, I'm going to search the logging path for the last-logged
+			#and delete it if it isn't relevant
+			path = os.path.abspath(os.getcwd()) + '/train_log'
+			list_subfolders_with_paths = [f.path for f in os.scandir(path) if f.is_dir()]
+			paths = [int(f.split('/')[-1].split('_')[-1]) for f in list_subfolders_with_paths] #Can combine with previous line, but wanted readability
+			try:
+				shutil.rmtree(path + '/PPO1_' + str(sorted(paths)[-1]))
+			except OSError as e:
+				print("Error removing log file")
+		return reward #Optuna by default minimizes, so changing this to positive distance
+	except KeyboardInterrupt:
+		raise Exception
 
 if __name__ == "__main__":
 	graph = None
 	clusters = None
+	pos = None
 	#This might be lazy code, but I think it is not worth importing more modules just to check if file exists before trying to open it
-	if os.path.isfile('clusters.pickle') and os.path.isfile('graph.gpickle'):
+	if os.path.isfile('clusters.pickle') and os.path.isfile('graph.gpickle') and os.path.isfile('position.pickle'):
+		print("Found graph from file, using saved graph")
 		clusters = pickle.load(open('clusters.pickle', 'rb'))
+		pos = pickle.load(open('position.pickle', 'rb'))
 		graph = nx.read_gpickle('graph.gpickle')
 	else:
+		print("Generating graph")
 		graph, clusters, pos = generateGraph(3, 45, draw=False)
+
 	try:
 		#I store the results in a SQLite database so that it can resume from checkpoints.
 		study = optuna.create_study(study_name='ppo_direct', storage='sqlite:///params_direct.db', load_if_exists=True)
-		study.optimize(lambda trial: optimize_algorithm(trial, graph, clusters), n_trials=500)
-	except KeyboardInterrupt:
+		study.optimize(lambda trial: optimize_algorithm(trial, graph, clusters, pos), n_trials=500)
+	except:
 		print('Interrupted, saving . . . ')
 		nx.write_gpickle(graph, 'graph.gpickle')
 		pickle.dump(clusters, open('clusters.pickle', 'wb'))
+		pickle.dump(pos, open('position.pickle', 'wb'))
 #Training without Optuna, so that we can compare the trained model to best possible controllers
 #if __name__ == "__main__":
 #	graph, clusters, pos = generateGraph(3, 45, draw=False)	#Generate graph
