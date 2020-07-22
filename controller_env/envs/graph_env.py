d646743270db11ec07d12a07bc9d9eb2a838bec2
vincentviray01 <vincentviray01@gmail.com>
2020-07-22 18:43:31 +0000

    updated heuristic and graph generation

--- controller_env/envs/graph_env.py
+++ controller_env/envs/graph_env.py
@@ -253,27 +253,67 @@ class ControllerEnv(gym.Env):
 
 	def graphCentroidAction(self) -> list:
 		"""Heuristic function, picks controllers based on centroid"""
-		actions = []
-		centroid = self.findGraphCentroid()[0]
-		print("CENTROID: ", centroid)
+		actions = []  # Set of Controllers
+		centroid = self.findGraphCentroid()[0]  # Graph Centroid
+		# Search each cluster for a controller to add to the actions variable
 		for index, cluster in enumerate(self.clusters):
+			# if the centroid is in the cluster being iterated over, skip that cluster
 			if self.graph.nodes[centroid]['cluster'] == index:
 				continue
+
 			bestNode = None
 			lowestDistance = 100000000
+			# search for node in cluster with smallest distance to centroid node
 			for node in cluster:
-				if nx.shortest_path_length(self.graph, centroid, node, weight = 'weight') < lowestDistance:
-					lowestDistance = nx.shortest_path_length(self.graph, centroid, node, weight = 'weight')
+				if nx.shortest_path_length(self.graph, centroid, node, weight='weight') < lowestDistance:
+					lowestDistance = nx.shortest_path_length(self.graph, centroid, node, weight='weight')
 					bestNode = node
 			actions.append(bestNode)
+
 		bestNode = None
 		lowestDistance = 10000000
+		# search for best node in the cluster where the centroid node lies within
 		for node in self.clusters[self.graph.nodes[centroid]['cluster']]:
 			if self.calculateDistance(actions + [node]) < lowestDistance:
 				lowestDistance = self.calculateDistance(actions + [node])
 				bestNode = node
 		actions.append(bestNode)
-		return actions
+
+		# Simulated Annealing Meta Heuristic
+		current_state = actions
+		annealing_rate = 0.90
+		for x in range(1000):
+			temperature = 1
+			# choose a random already-selected node
+			randomCluster = np.random.randint(len(self.clusters))
+			randomBestAction = current_state[randomCluster]
+			# get the neighbors of the randomly selected controller
+			neighborList = [v for k, v in self.graph.edges if
+							(k == randomBestAction and v in self.clusters[
+								self.graph.nodes[randomBestAction]['cluster']]) or
+							(v == randomBestAction and k in self.clusters[
+								self.graph.nodes[randomBestAction]['cluster']])]
+			neighborList.append(randomBestAction)
+			# Get a random neighbor
+			randomNeighbor = np.random.choice(neighborList)
+			# create an alternate controller list with the neighbor replacing the previous controller
+			proposed_state = current_state.copy()
+			proposed_state[randomCluster] = randomNeighbor
+			# get a random float between 0 and 1 to be the probability of changing the controller list
+			threshold = float(np.random.rand(1))
+			proposed_distance = self.calculateDistance(proposed_state)
+			current_distance = self.calculateDistance(current_state)
+			# if the new controller list is better than the original controller list, change to the new controller list
+			# else change to the worst controller list with probability e^(-(new_distance - old_distance) / temperature)
+			probability = 1 if proposed_distance < current_distance else np.exp(
+				-(proposed_distance - current_distance) / temperature)
+			if probability >= threshold:
+				current_state = proposed_state.copy()
+			temperature *= annealing_rate
+		# ensure that the function returns the best of the original heuristic or the SA version
+		if self.calculateDistance(actions) < self.calculateDistance(current_state):
+			current_state = actions
+		return current_state, self.calculateDistance(current_state)
 
 
 def generateGraph(num_clusters: int, num_nodes: int, prob_cluster: float=0.5, prob: float=0.2, weight_low: int=0, weight_high: int=100, draw=True) -> (nx.Graph, list, dict):
@@ -359,99 +399,138 @@ def generateGraph(num_clusters: int, num_nodes: int, prob_cluster: float=0.5, pr
 
 
 def generateAlternateGraph(num_clusters: int, num_nodes: int, weight_low: int = 0, weight_high: int = 100,
-						   draw=True) -> (nx.Graph, list, dict):
-	"""
-	Generates graph given number of clusters and nodes
-	Args:
-		num_clusters: Number of clusters
-		num_nodes: Number of nodes
-		weight_low: Lowest possible weight for edge in graph
-		weight_high: Highest possible weight for edge in graph
-		draw: Whether or not to show graph (True indicates to show)
+                           draw=True) -> (nx.Graph, list, dict):
+    """
+    Generates graph given number of clusters and nodes
+    Args:
+        num_clusters: Number of clusters
+        num_nodes: Number of nodes
+        weight_low: Lowest possible weight for edge in graph
+        weight_high: Highest possible weight for edge in graph
+        draw: Whether or not to show graph (True indicates to show)
 
-	Returns:
-		Graph with nodes in clusters, array of clusters, graph position for drawing
-	"""
-	node_colors = np.arange(0, num_nodes, 1, np.uint8)  # Stores color of nodes
-	nodes_per_cluster = int(num_nodes / num_clusters)
-	clusters = np.zeros((num_clusters, nodes_per_cluster), np.uint8)  # Stores nodes in each cluster
-	node_colors = node_colors // nodes_per_cluster  # Groups the node_colors
-	G = nx.Graph()
+    Returns:
+        Graph with nodes in clusters, array of clusters, graph position for drawing
+    """
+    node_colors = np.arange(0, num_nodes, 1, np.uint8)  # Stores color of nodes
+    total_nodes = 0
+    temp_clusters = {}  # Temp storage for clusters
+    remainder = num_nodes % num_clusters
+    clusters = []  # Stores nodes in each cluster
+    # organize number of nodes per cluster and assign node colors
+    temp = 0
+    # fill in cluster and temp cluster variables and set up node_colors variable
+    for x in range(num_clusters):
+        if remainder > x:
+            nodes_per_cluster = int(num_nodes / num_clusters) + 1
+        else:
+            nodes_per_cluster = int(num_nodes / num_clusters)
 
-	cluster_endpoints = []
-	cluster = nx.full_rary_tree(int(np.log2(nodes_per_cluster)), nodes_per_cluster)
-	temp = 0  # variable used to ensure diameter is as small as possible
-	while nx.diameter(cluster) > (np.log2(nodes_per_cluster) + temp):
-		cluster = nx.full_rary_tree(int(np.log2(nodes_per_cluster)), nodes_per_cluster)
-		temp += 1
-	nx.set_node_attributes(cluster, 0, 'cluster')
-	clusters[0, :] = np.asarray(cluster.nodes)
+        temp_clusters[x] = nodes_per_cluster
+        node_colors[temp + np.arange(nodes_per_cluster)] = x
+        temp += nodes_per_cluster
+        clusters.append(list(np.arange(nodes_per_cluster) + total_nodes))
+        total_nodes += nodes_per_cluster
+    G = nx.Graph()
 
-	inner_cluster_edges = np.random.randint(0, nodes_per_cluster,
-											(int(np.log2(nodes_per_cluster)), 2))
-	cluster.add_edges_from(inner_cluster_edges)
+    cluster_endpoints = []
 
-	G = nx.disjoint_union(G, cluster)
-	for i in range(1, num_clusters):
-		cluster = nx.full_rary_tree(int(np.log2(nodes_per_cluster)), nodes_per_cluster)
-		temp = 0
-		while nx.diameter(cluster) > (np.log2(nodes_per_cluster) + temp):
-			cluster = nx.full_rary_tree(int(np.log2(nodes_per_cluster)), nodes_per_cluster)
-			temp += 1
+    # create first cluster
+    cluster = nx.full_rary_tree(int(np.log2(temp_clusters[0])), temp_clusters[0])
 
-		nx.set_node_attributes(cluster, i, 'cluster')
-		clusters[i, :] = np.asarray(cluster.nodes) + nodes_per_cluster * i
-		G = nx.disjoint_union(G, cluster)
-		cluster_endpoint = np.random.randint(0, nodes_per_cluster)
-		cluster_endpoints.append(cluster_endpoint)
-		G.add_edge(cluster_endpoint, np.random.randint(nodes_per_cluster * i, nodes_per_cluster * i + i))
+    temp = 0  # variable used to ensure diameter is as small as possible
+    while nx.diameter(cluster) > (np.log2(temp_clusters[0]) + temp):
+        cluster = nx.full_rary_tree(int(np.log2(temp_clusters[0])),temp_clusters[0])
+        temp += 1
+    nx.set_node_attributes(cluster, 0, 'cluster')
 
-	# adding inter and inner edges of the clusters
-	closest_length = 1000
-	nearest_cluster = 0
-	shortest_path = 0
-	for i in range(1, num_clusters):
-		# check for closest cluster besides main cluster
-		for x in range(2, num_clusters - 1):
-			shortest_path = nx.shortest_path_length(G, cluster_endpoints[i - 1], cluster_endpoints[x - 1])
-			if shortest_path < closest_length:
-				closest_length = shortest_path
-				nearest_cluster = x
+    # set initial edge weight of first cluster
+    for (u, v) in cluster.edges():
+        cluster.edges[u, v]['weight'] = np.random.random() * 0.75 * (weight_high - weight_low) + weight_low * 1.25
 
-		# add inner_cluster_edges
-		inner_cluster_edges = np.random.randint(nodes_per_cluster * i, nodes_per_cluster * i + nodes_per_cluster,
-												(int(np.log2(nodes_per_cluster)), 2))
-		G.add_edges_from(inner_cluster_edges)
+    inner_cluster_edges = np.random.randint(0, temp_clusters[0],
+                                            (int(np.log2(temp_clusters[0])), 2))
 
-		# if the nearest_cluster is too far away, don't add inter-cluster edges
-		if shortest_path > (np.random.randint(np.log2(nodes_per_cluster), np.log2(nodes_per_cluster) + 1)):
-			continue
+    # add edge weights to new edges of first cluster
+    inner_cluster_edges =  [(u, v,  np.random.random() * 0.75 * (weight_high - weight_low) + weight_low * 1.25) for u,v in inner_cluster_edges]
+    cluster.add_weighted_edges_from(inner_cluster_edges)
 
-		# add inter_cluster_edges
-		inter_cluster_edges = np.random.randint(nodes_per_cluster * i, nodes_per_cluster * i + nodes_per_cluster,
-												(int(nodes_per_cluster / (
-															np.random.randint(0, (np.log2(nodes_per_cluster))) + 1))))
-		inter_cluster_edges = [[y, np.random.randint(nodes_per_cluster * nearest_cluster,
-													 nodes_per_cluster * nearest_cluster + nodes_per_cluster)] for y in
-							   inter_cluster_edges]
+    G = nx.disjoint_union(G, cluster)
 
-		G.add_edges_from(inter_cluster_edges)
-	G.remove_edges_from(nx.selfloop_edges(G))  # Remove self-loops caused by adding random edge
+    # create other clusters
+    for i in range(1, num_clusters):
+        # create cluster
+        cluster = nx.full_rary_tree(int(np.log2(temp_clusters[i])), temp_clusters[i])
+        temp = 0
+        while nx.diameter(cluster) > (np.log2(temp_clusters[i]) + temp):
+            cluster = nx.full_rary_tree(int(np.log2(temp_clusters[i])), temp_clusters[i])
+            temp += 1
 
-	# Add edge weights to the whole graph
-	for (u, v) in G.edges():
-		G.edges[u, v]['weight'] = np.random.random() * (weight_high - weight_low) + weight_low
-	pos = nx.spring_layout(G)
+        nx.set_node_attributes(cluster, i, 'cluster')
 
-	# Draw graph
-	if draw:
-		nx.draw_networkx_nodes(G, pos, node_color=node_colors)
-		nx.draw_networkx_labels(G, pos)
-		nx.draw_networkx_edges(G, pos, G.edges())
-		plt.draw()
-		plt.show()
+        # set initial edge weights
+        for (u, v) in cluster.edges():
+            if not(u in np.arange(temp_clusters[x]/2) or v in np.arange(temp_clusters[x] / 2)):
+                cluster.edges[u, v]['weight'] = np.random.random() * 0.20 * (weight_high - weight_low) + weight_low * 1.05
+            else:
+                cluster.edges[u, v]['weight'] = np.random.random() * 0.05 * (weight_high - weight_low) + weight_low
 
-	return G, clusters, pos
+
+        G = nx.disjoint_union(G, cluster)
+
+        # add connections from new clusters to first cluster
+        cluster_endpoint = np.random.randint(0, temp_clusters[i])
+        cluster_endpoints.append(cluster_endpoint)
+        G.add_edge(cluster_endpoint, np.random.randint(temp_clusters[i] * i, temp_clusters[i] * i + i), weight = np.random.random() * 0.20 * (weight_high - weight_low) + weight_low * 1.05)
+
+    # adding inter and inner edges of the clusters
+    closest_length = 1000
+    nearest_cluster = 0
+    shortest_path = 0
+    for i in range(1, num_clusters):
+        # check for closest cluster besides main cluster
+        for x in range(2, num_clusters - 1):
+            shortest_path = nx.shortest_path_length(G, cluster_endpoints[i - 1], cluster_endpoints[x - 1])
+            if shortest_path < closest_length:
+                closest_length = shortest_path
+                nearest_cluster = x
+
+        # add inner_cluster_edges
+        inner_cluster_edges = np.random.randint(temp_clusters[i] * i, temp_clusters[i] * i + temp_clusters[i],
+                                                (int(np.log2(temp_clusters[i])), 2))
+        inner_cluster_edges = [(u, v, np.random.random() * 0.05 * (weight_high - weight_low) + weight_low) for
+                               u, v in inner_cluster_edges]
+        # cluster.add_weighted_edges_from(inner_cluster_edges)
+        G.add_weighted_edges_from(inner_cluster_edges)
+
+        # if the nearest_cluster is too far away, don't add inter-cluster edges
+        if shortest_path > (np.random.randint(np.log2(temp_clusters[i]), np.log2(temp_clusters[i]) + 1)):
+            continue
+
+        # add inter_cluster_edges
+        inter_cluster_edges = np.random.randint(temp_clusters[i] * i, temp_clusters[i] * i + temp_clusters[i],
+                                                (int(temp_clusters[i] / (
+                                                            np.random.randint(0, (np.log2(temp_clusters[i]))) + 1))))
+        inter_cluster_edges = [[y, np.random.randint(temp_clusters[i] * nearest_cluster,
+                                                     temp_clusters[i] * nearest_cluster + temp_clusters[i]), np.random.random() * 0.20 * (weight_high - weight_low) + weight_low * 1.05] for y in
+                               inter_cluster_edges]
+
+        # cluster.add_weighted_edges_from(inner_cluster_edges)
+        G.add_weighted_edges_from(inter_cluster_edges)
+    G.remove_edges_from(nx.selfloop_edges(G))  # Remove self-loops caused by adding random edge
+
+    pos = nx.spring_layout(G)
+
+    # Draw graph
+    if draw:
+        nx.draw_networkx_nodes(G, pos, node_color=node_colors)
+        nx.draw_networkx_labels(G, pos)
+        # nx.draw_networkx_edge_labels(G, pos)
+        nx.draw_networkx_edges(G, pos, G.edges())
+        plt.draw()
+        plt.show()
+
+    return G, clusters, pos
 
 def generateClusters(graph: nx.Graph, edge_label: str=None) -> (nx.Graph, list, dict):
 	"""
